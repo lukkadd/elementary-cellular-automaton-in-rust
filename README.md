5a9a15f387249a28eca2c5685910350342282717
Lucca Debbio <lucca.debbio@perallis.com>
2022-05-29 03:32:42 -0300

    wrote a function to generate images using the ppm format specifications

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# elementary-cellular-automaton-in-rust
+# Elementary Celular Automaton
+This is a simple implementation of an elementary celular automaton generator in rust.
+Given an initial state, a rule and a number of iterations it will output an image with the visual representation of the automaton.
+
+## How to build/run
+This project uses cargo, so to run in development mode use `cargo run`.
+And to generate an optimized build use `cargo build --release`.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::fs::File;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    create_ppm("./examples/image-test.ppm", 3, 1, &vec!(0xFF00FF, 0x00FFFF, 0xFFFF00));
+}
+
+/** Create PPM image file */
+fn create_ppm(path: &str, width: usize, height: usize, pixel_data: &[i32]) {
+	// Creating the file
+	let mut image_file = File::create(path).expect("Unable to create file");
+
+	//Writing PPM header (rules 1 through 8) 
+	write!(image_file, "P6\n{} {}\n255\n", width, height).expect("Unable to write to file");
+
+	//Writing pixel data 
+	for pixel in pixel_data {
+		let color: [u8; 3] = [(pixel >> 16) as u8, (pixel >> 8) as u8, (pixel >> 0) as u8];
+		image_file.write_all(&color).expect("Unable to write to file");
+	}
 }
