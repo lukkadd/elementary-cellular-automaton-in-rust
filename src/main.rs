5ee59febf9e81c0e4b007bba954a14a918b6eb2a
Lucca Debbio <lucca.debbio@perallis.com>
2022-05-29 03:48:39 -0300

    Had to write a conversion between the automaton generated boolean vector and the i32 color vector the ppm image expects

--- src/main.rs
+++ src/main.rs
@@ -21,9 +21,13 @@ fn main() {
     //Generating ECA data over multiple generations 
     let eca_data = eca(&initial_state, rule30, ITERATIONS);
     println!("{:?}", eca_data);
+
+    //boolean to color conversion test
+    let color_data = bool_to_color(&eca_data);
+    println!("{:?}", color_data);
 }
 
-/** Create PPM image file */
+// Create PPM image file
 fn create_ppm(path: &str, width: usize, height: usize, pixel_data: &[i32]) {
 	// Creating the file
 	let mut image_file = File::create(path).expect("Unable to create file");
@@ -38,10 +42,21 @@ fn create_ppm(path: &str, width: usize, height: usize, pixel_data: &[i32]) {
 	}
 }
 
+// Convert bool vector to i32 vector where false means white and true means black 
+fn bool_to_color (bool_data: &Vec<bool>) -> Vec<i32> {
+	let mut color_data: Vec<i32> = Vec::new();
+	for i in 0..bool_data.len() {
+		color_data.push(if bool_data[i] { 0x0 } else {0xFFFFFF});
+	}
+	return color_data;
+}
+
+// ECA rule 30 defined in a boolean function
 fn rule30 (p: bool, q:bool, r: bool) -> bool{
 	(!p && r) || (!p && q) || (p && !q && !r)
 }
 
+// Generate ECA data based on a given initial state, rule and number of iterations
 fn eca (initial_state: &Vec<bool>, rule: fn(bool, bool, bool) -> bool, iterations: usize) -> Vec<bool> {
 	let mut result: Vec<bool> = Vec::new();
 	let number_of_cells = initial_state.len();
@@ -55,6 +70,7 @@ fn eca (initial_state: &Vec<bool>, rule: fn(bool, bool, bool) -> bool, iteration
 	return result;
 }
 
+// Generate a single iteration of the ECA
 fn eca_step (current_state: &[bool], rule: fn(bool, bool, bool)->bool) -> Vec<bool> {
 	if current_state.len() < 3 {panic!("length {}", current_state.len())} //Must have at least 3 cells for the rule to apply
 	
