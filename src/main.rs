bb48de9261597028a8877504938ad0117a4338b9
Lucca Debbio <lucca.debbio@perallis.com>
2022-05-29 03:37:08 -0300

    Wrote a function to enforce cellular automaton rule 30 (https://www.wolframalpha.com/input?i=rule+30)

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,11 @@ use std::fs::File;
 use std::io::Write;
 
 fn main() {
+    //image creation test
     create_ppm("./examples/image-test.ppm", 3, 1, &vec!(0xFF00FF, 0x00FFFF, 0xFFFF00));
+    
+    //rule 30 test
+    println!("{}",rule30(true, false, true));
 }
 
 /** Create PPM image file */
@@ -18,4 +22,8 @@ fn create_ppm(path: &str, width: usize, height: usize, pixel_data: &[i32]) {
 		let color: [u8; 3] = [(pixel >> 16) as u8, (pixel >> 8) as u8, (pixel >> 0) as u8];
 		image_file.write_all(&color).expect("Unable to write to file");
 	}
+}
+
+fn rule30 (p: bool, q:bool, r: bool) -> bool{
+	(!p && r) || (!p && q) || (p && !q && !r)
 }
